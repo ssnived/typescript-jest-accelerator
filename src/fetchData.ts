39c8902d53568d73129d4faa2e47aa16a0a77e76
ssnived <ssniv@hotmail.com>
2024-01-15 19:29:24 +1100

    deleted unused fetch data file

--- src/fetchData.ts
+++
@@ -1,7 +0,0 @@
-// sample api call
-export const fetchData = async () => {
-  const response = await fetch("https://api.example.com/data");
-  const data = await response.json();
-  console.log("Response Payload:", data);
-  return data;
-};
